3d1d7ed5756562139abfce0007334e9cd5661fd3
Priyashri <priyashri160208@gmail.com>
2026-06-30 10:35:42 +0530

    Added CountryService.java

---
+++ SpringDataJPA/CountryService.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
