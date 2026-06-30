c98000fc47324ed200dd416aefc96f947040075f
Priyashri <priyashri160208@gmail.com>
2026-06-30 10:57:01 +0530

    Added HQL and Native Query methods

--- SpringDataJPA/CountryRepository.java
+++ SpringDataJPA/CountryRepository.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +12,10 @@ public interface CountryRepository extends JpaRepository<Country, String> {
     List<Country> findByNameContaining(String keyword);
 
     List<Country> findByNameStartingWith(String prefix);
+
+    @Query("SELECT c FROM Country c WHERE c.name = ?1")
+    List<Country> findCountryByNameHQL(String name);
+
+    @Query(value = "SELECT * FROM country WHERE co_name = ?1", nativeQuery = true)
+    List<Country> findCountryByNameNative(String name);
 }
