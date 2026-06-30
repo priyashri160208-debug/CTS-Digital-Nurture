f84b2776098b8fc23d97c211bf34da2428b39627
Priyashri <priyashri160208@gmail.com>
2026-06-30 10:53:43 +0530

    Added Spring Data JPA query methods

--- SpringDataJPA/CountryRepository.java
+++ SpringDataJPA/CountryRepository.java
@@ -1,7 +1,14 @@
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, String> {
 
+    List<Country> findByName(String name);
+
+    List<Country> findByNameContaining(String keyword);
+
+    List<Country> findByNameStartingWith(String prefix);
 }
