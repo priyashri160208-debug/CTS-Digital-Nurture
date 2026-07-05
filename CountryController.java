296561d2765a5cda6483f18b1f421e72d63e75af
Priyashri <priyashri160208@gmail.com>
2026-07-05 21:34:08 +0530

    Updated CountryController for country code search

--- CountryController.java
+++ CountryController.java
@@ -1,22 +1,26 @@
 package com.cognizant.spring_learn.controller;
 
 import com.cognizant.spring_learn.Country;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @RequestMapping("/country")
     public Country getCountryIndia() {
+        return countryService.getCountry("IN");
+    }
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
-
-        return country;
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
     }
 }
